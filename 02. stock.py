9fc31295347d0740eb8185056942164235e61e9d
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-01 20:49:01 +0100

    03 begin

--- 02. stock.py
+++ 02. stock.py
@@ -4,7 +4,7 @@ products_information = input().split()
 
 stock_date = {}
 
-for i in range(o, len(products_information), 2):
+for i in range(0, len(products_information), 2):
     product = products_information[i]
     quantity = products_information[i + 1]
     stock_date[product] = quantity
@@ -16,7 +16,7 @@ for current_product in products_search:
     if current_product in stock_date:
         print(f"We have {stock_date[current_product]} of {current_product} left")
     else:
-        print(f"Sorry, we don\'t have the product")        
+        print(f"Sorry, we don\'t have the {current_product}")        
 
 # Second task from me
 
--- 03. statistics.py
+++ 03. statistics.py
@@ -1,6 +1,22 @@
 # first task from the lecture
 
+stock = {}
 
+while True:
+    line = input()
+    
+    if line == "statistics":
+        break    
 
+    product, quantity = line.split(": ")
+    
+    if product in stock:
+        stock[product] += quantity
+    else:
+        stock[product] = quantity
+        
+        
+            
+            
 # Second task from me
 
