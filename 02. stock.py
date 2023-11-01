2eb90a435e3125a324003a2c3efa0ff09443a610
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-01 20:37:33 +0100

    02

--- 02. stock.py
+++ 02. stock.py
@@ -1,6 +1,15 @@
 # first task from the lecture
 
+products_information = input().split()
 
+stock_date = {}
+
+for i in range(o, len(products_information), 2):
+    product = products_information[i]
+    quantity = products_information[i + 1]
+    stock_date[product] = quantity
+    
+    
 
 # Second task from me
 
