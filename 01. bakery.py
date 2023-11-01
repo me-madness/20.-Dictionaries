2e0b03f9572ead5d63e275ffa8960d90dee3b0a8
me_madness <p_rangel@abv.bg>
2023-11-01 18:58:53 +0100

    01. it's done by the lecture

--- 01. bakery.py
+++ 01. bakery.py
@@ -1,6 +1,14 @@
 # first task from the lecture
 
+data = input().split()
 
+stock = {}
 
+for i in range(0, len(data), 2):
+    food = data[i]
+    quantity = data[i + 1]
+    stock[food] = quantity
+    
+print(stock)    
 # Second task from me
 
