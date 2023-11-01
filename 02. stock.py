4468eb460f8bc7982fc2f05899e54a9a1076f088
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-01 20:40:31 +0100

    02. Done by the lecture

--- 02. stock.py
+++ 02. stock.py
@@ -9,7 +9,13 @@ for i in range(o, len(products_information), 2):
     quantity = products_information[i + 1]
     stock_date[product] = quantity
     
-    
+product_search = input().split()
+
+for current_product in product_search:
+    if current_product in stock_date:
+        print(f"We have {stock_date[current_product]} of {current_product} left")
+    else:
+        print(f"Sorry, we don't have the product")        
 
 # Second task from me
 
