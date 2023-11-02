2e30c3860f897efc8ee218bc5289c4e08ca101bf
me-madness <58656549+me-madness@users.noreply.github.com>
2023-11-02 17:56:46 +0100

    all most done

--- 04. students.py
+++ 04. students.py
@@ -1,7 +1,23 @@
 # first task from the lecture
 
 students = []
+course_to_search = None
 
+while True:
+    student_info = input()
+    
+    if ":" no in student_info:
+        course_to_search = student_info
+        break
+    
+    
+    name, ID, course = student_info.split(":")
+    students.append({'name': name, 'ID': ID, 'course': course})
+    
+    
+for student in students:
+    if course_to_search.startswith(studen['course'][0:3]):
+            
 
 # Second task from me
 
